7c1fabef1985fe57f87769353f5bb9d0fe290ddd
Naziya <naziyasyeda11@gmail.com>
2021-07-30 01:55:06 +0530

    [Naziya]Add: logger format

---
+++ Matplotlib/Scatterplot/LoggerFormat.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging
+   
+logger.basicConfig(filename='scatterplot.log', level=logging.INFO,
+                    format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+logger.basicConfig(filename='scatterplot.log', level=logging.ERROR,
+                    format='%(asctime)s:%(funcName)s:%(levelname)s:%(lineno)d')
